df6e09e66499e5fa00052da4f45b4d1986449aeb
Jomar <jomar.magno026@gmail.com>
2019-01-01 16:00:09 +0800

    connect to the backend

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import uuid from 'uuid';
 
 class ItemModal extends Component {
    state = {
@@ -20,7 +19,6 @@ class ItemModal extends Component {
       e.preventDefault();
       
       const newItem = {
-         id: uuid(),
          name: this.state.name
       };
 
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -38,15 +38,15 @@ class ShoppingList extends Component {
                <ListGroup>
                   <TransitionGroup className="shopping-list">
                      {
-                        items.map( ({id, name}) => {
+                        items.map( ({_id, name}) => {
                            return (
-                              <CSSTransition key={id} timeout={500} classNames="fade">
+                              <CSSTransition key={_id} timeout={500} classNames="fade">
                                  <ListGroupItem>
                                     <Button 
                                        className="mr-3"
                                        color="danger"
                                        size="sm"
-                                       onClick={this.deleteItemHandler.bind(this, id)}
+                                       onClick={this.deleteItemHandler.bind(this, _id)}
                                     >
                                        &times;
                                     </Button>
