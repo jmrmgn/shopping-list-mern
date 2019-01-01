c73870fb8e76bd9feddb12926ad402f973732c6f
Jomar <jomar.magno026@gmail.com>
2019-01-01 16:16:28 +0800

    deployment setup

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -14,6 +16,16 @@ app.use(bodyParser.json());
 
 // Use routes
 app.use('/api/items', itemRoutes);
+
+// Serve static assets if int production
+if (process.env.NODE_ENV == 'production') {
+   // Set static folder
+   app.use(express.static('client/build'));
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
 
 // DB Config
 mongoose.connect(
